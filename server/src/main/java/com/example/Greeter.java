5470c00fccbcce17a3b2e6a135a963bd16d98263
nagarjunareddyg <gnagarjunareddy19@gmail.com>
2020-08-17 07:35:03 -0700

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -3,17 +3,17 @@ package com.example;
 /**
  * This is a class.
  */
-public class Greeter {
+//public class Greeter {
 
   /**
    * This is a constructor.
    */
-  public Greeter() {
+  //public Greeter() {
 
-  }
+  //}
 
   //TODO: Add javadoc comment
- // public String greet(String someone) {
-  //  return String.format("Hello, %s!", someone);
-  //}
-}
+  //public String greet(String someone) {
+   //return String.format("Hello, %s!", someone);
+ //}
+//}
